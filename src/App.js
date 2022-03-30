3cf50b33e445be040e4ce3296ecad1d665adc2eb
Matthew-Clay <matthewclay.dev@gmail.com>
2022-03-30 22:09:05 +0630

    error while arranging data from API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Question from "./components/Question";
 import "./App.css";
 
 function App() {
-  const [isStart, setStart] = useState(false); // state for changing after start-btn click
+  // state
+  const [welcome, setWelcome] = useState(true); // state for welcome page
+  const [question, setQuestion] = useState([]); // state for quizzes
+  const [checked, setChecked] = useState(false); // state for checking answers
+  const [score, setScore] = useState(0); // state for user's score
 
-  // const [quiz, setQuiz] = useState([]); // fetch data from trivia_api and set them to quiz
-  // const [score, setScore] = useState([]); // score state for result
-
-  // ------- functions -------
+  console.log(question);
 
   const handleClick = () => {
-    setStart((prevVal) => !prevVal);
+    setWelcome((oldValue) => !oldValue); // for welcome page onClick
   };
 
-  // -------  for component -------
+  useEffect(() => {
+    const url = "https://opentdb.com/api.php?amount=5&type=multiple";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setQuestion(data.results));
+  }, []);
 
   const welcomePage = (
     <section className="welcome">
@@ -27,9 +35,50 @@ function App() {
     </section>
   );
 
-  const quizComponent = "";
+  const runHold = () => {
+    console.log("run hold...");
+  };
 
-  return <main>{!isStart ? welcomePage : quizComponent}</main>;
+  const questionElements = question.map((questionArr) => {
+    return (
+      <Question
+        key={questionArr.id}
+        question={questionArr.question}
+        // answer={questionArr.icorrect_answers} // this will update soon
+        id={questionArr.id}
+        runHold={runHold}
+      />
+    );
+  });
+
+  const checkAnswers = () => {
+    console.log("checking answers...");
+  };
+
+  const newGame = () => {
+    console.log("new game...");
+  };
+
+  return (
+    <main className="app">
+      {welcome && welcomePage}
+      <section className="quiz-container">
+        {questionElements}
+        <div className="btn-container">
+          {checked ? (
+            <div>
+              <span className="score-container">
+                You scored {score}/{question.length} correct answers
+                <button onClick={newGame}>Play Again</button>
+              </span>
+            </div>
+          ) : (
+            <button onClick={checkAnswers}>Check Answers</button>
+          )}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
